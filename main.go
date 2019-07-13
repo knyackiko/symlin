1286cf77ba318124edd5fee1e5f753144fa64b77
kyklades <kyklades.islands@gmail.com>
2019-07-14 03:33:16 +0900

    Use cli package.

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"log"
 	"os"
-	"os/user"
 	"path/filepath"
-	"strings"
+	"sort"
 
 	"github.com/fatih/color"
+	"github.com/urfave/cli"
 )
 
 type symbolicLink struct {
@@ -17,13 +17,15 @@ type symbolicLink struct {
 	actualPath string
 }
 
-var (
-	listOption          bool
-	dirOption           string
-	unlinkOption        bool
-	targetSymlinkOption string
+const (
+	listCommand   string = "list"
+	createCommand string = "create"
+	unlinkCommand string = "unlink"
+)
 
-	homeDir string
+var (
+	isRelative bool
+	currentDir string
 
 	// color
 	yellow = color.New(color.FgYellow).SprintFunc()
@@ -31,54 +33,81 @@ var (
 )
 
 func init() {
-	user, err := user.Current()
+	var err error
+	currentDir, err = os.Getwd()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
+		os.Exit(1)
 	}
-	homeDir = user.HomeDir
-
-	flag.BoolVar(&listOption, "list", false, "List up symbolic links in a target directory.")
-	flag.StringVar(&dirOption, "dir", homeDir, "Set a target directory path.")
-	flag.BoolVar(&unlinkOption, "unlink", false, "Unlink a target symbolic link.")
-	flag.StringVar(&targetSymlinkOption, "target", "", "Set a target symbolic link to unlink.")
-	flag.Parse()
 }
 
 func main() {
-	var err error
-	err = formatPaths()
+	app := cli.NewApp()
+	app.Name = "Symlin"
+	app.Version = "1.0.0"
+	app.Author = "kyklades"
+	app.Usage = "Symbolic links manager."
+	app.Description = "This manages symbolic links."
+	app.Commands = getCommands()
+	sort.Sort(cli.CommandsByName(app.Commands))
+	err := app.Run(os.Args)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	err = execute()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	} else {
-		os.Exit(0)
+		log.Fatal(err)
 	}
 }
 
-func execute() error {
-	var err error
-	switch {
-	case listOption && dirOption != "":
-		err = printSymbolicLinks()
-	case unlinkOption && targetSymlinkOption != "":
-		err = unlink()
-		if err == nil {
-			fmt.Printf("%s has been successfully unlinked!\n", yellow(targetSymlinkOption))
+func getCommands() []cli.Command {
+	return []cli.Command{
+		{
+			Name:        listCommand,
+			Aliases:     []string{"l"},
+			Usage:       "List up symbolic links.",
+			ArgsUsage:   "[target dir]",
+			Description: "List up symbolic links in a target directory. Default target directory is the current directory.",
+			Action:      doListUp,
+		},
+		{
+			Name:        createCommand,
+			Aliases:     []string{"c"},
+			Usage:       "Create a symbolic link.",
+			ArgsUsage:   "[target path] [new path]",
+			Description: "Create a symbolic link.",
+			Action:      doCreate,
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:        "relative, r",
+					Usage:       "if true, the type of symbolic link is relative symbolic link.",
+					Destination: &isRelative,
+				},
+			},
+		},
+		{
+			Name:        unlinkCommand,
+			Aliases:     []string{"u"},
+			Usage:       "Unlink a target symbolic link.",
+			ArgsUsage:   "[target symbolic link]",
+			Description: "Unlink a target symbolic link.",
+			Action:      doUnlink,
+		},
+	}
+}
+
+func doListUp(c *cli.Context) (err error) {
+	var symbolicLinks []symbolicLink
+	switch len(c.Args()) {
+	case 0:
+		symbolicLinks, err = listUp(currentDir)
+	case 1:
+		var targetDirPath string
+		targetDirPath, err = formatPath(c.Args().Get(0))
+		if err != nil {
+			return nil
 		}
+		symbolicLinks, err = listUp(targetDirPath)
 	default:
-		flag.PrintDefaults()
+		return
 	}
-	return err
-}
 
-func printSymbolicLinks() error {
-	symbolicLinks, err := listUpSymbolicLinks()
 	if symbolicLinks != nil {
 		for _, symbolicLink := range symbolicLinks {
 			fmt.Printf("%s -> %s\n", yellow(symbolicLink.name), cyan(symbolicLink.actualPath))
@@ -87,21 +116,21 @@ func printSymbolicLinks() error {
 	return err
 }
 
-func listUpSymbolicLinks() ([]symbolicLink, error) {
+func listUp(dir string) ([]symbolicLink, error) {
 	var (
 		list []symbolicLink
 		err  error
 	)
 
-	err = filepath.Walk(dirOption, func(path string, info os.FileInfo, err error) error {
-		if path != dirOption && info.IsDir() {
+	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if path != dir && info.IsDir() {
 			return filepath.SkipDir
 		} else if info.Mode()&os.ModeSymlink == os.ModeSymlink {
 			actualPath, e := os.Readlink(path)
 			if e != nil {
 				return e
 			}
-			list = append(list, symbolicLink{filepath.Base(path), path, filepath.Join(filepath.Dir(path), actualPath)})
+			list = append(list, symbolicLink{filepath.Base(path), path, actualPath})
 		}
 		return nil
 	})
@@ -109,25 +138,63 @@ func listUpSymbolicLinks() ([]symbolicLink, error) {
 	return list, err
 }
 
-func unlink() error {
-	_, err := os.Lstat(targetSymlinkOption)
+func doCreate(c *cli.Context) (err error) {
+	if len(c.Args()) != 2 {
+		cli.ShowCommandHelp(c, createCommand)
+		return nil
+	}
+
+	var paths []string
+	paths, err = formatPaths(c.Args())
 	if err != nil {
 		return err
 	}
-	return os.Remove(targetSymlinkOption)
-}
 
-func formatPaths() error {
-	var err error
-	dirOption, err = formatPath(dirOption)
-	targetSymlinkOption, err = formatPath(targetSymlinkOption)
+	err = os.Symlink(paths[0], paths[1])
+	if err == nil {
+		fmt.Printf("New symbolic link has been created!\n%s -> %s\n", yellow(filepath.Base(paths[1])), cyan(paths[0]))
+	}
 	return err
 }
 
-func formatPath(path string) (formattedPath string, err error) {
-	if strings.Contains(path, "~") {
-		formattedPath = strings.Replace(path, "~", homeDir, 1)
+func doUnlink(c *cli.Context) (err error) {
+	if len(c.Args()) != 1 {
+		cli.ShowCommandHelp(c, unlinkCommand)
+		return nil
 	}
+
+	var target string
+	target, err = formatPath(c.Args().Get(0))
+	err = unlink(target)
+	if err == nil {
+		fmt.Printf("%s has been successfully unlinked!\n", yellow(target))
+	}
+	return err
+}
+
+func unlink(target string) error {
+	_, err := os.Lstat(target)
+	if err != nil {
+		return err
+	}
+	return os.Remove(target)
+}
+
+func formatPath(path string) (formattedPath string, err error) {
 	formattedPath = filepath.Clean(path)
+	if isRelative {
+		return path, nil
+	}
 	return filepath.Abs(path)
+}
+
+func formatPaths(paths []string) (formatPaths []string, err error) {
+	for _, v := range paths {
+		path, err := formatPath(v)
+		if err != nil {
+			return nil, err
+		}
+		formatPaths = append(formatPaths, path)
+	}
+	return formatPaths, nil
 }
